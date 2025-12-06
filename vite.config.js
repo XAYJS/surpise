f1f61bd2d12f64470c71d0eca3cd4e874a6d7e73
xay <xayderder@gmail.com>
2025-12-06 16:03:17 +0700

    Fix blank screen issue

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [tailwindcss(), tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
+      base: "/surpise/"
     }),
     react()
     ],
